9cf2d7297a0c392a8c917ac45d45e9930c1602ad
harithwick <harithsen@gmail.com>
2021-07-13 09:05:30 +1000

    Added Alchemist

--- lib/data/books_brain.dart
+++ lib/data/books_brain.dart
@@ -3,6 +3,14 @@ import 'package:minimal_portfolio_webapp/models/books_obj.dart';
 class BooksBrain {
   List<Book> booksBank = [
     Book(
+        heading: "The Alchemist",
+        disc:
+            "The story of Santiago, an Andalusian shepherd boy who travel in search of a worldly treasure.",
+        imagePath: 'assets/images/alchemist.jpg',
+        keytakeaway: "",
+        buyURL:
+            "https://www.amazon.com/Alchemist-Paulo-Coelho/dp/0062315005/ref=pd_lpo_14_t_2/137-2112040-3464843?_encoding=UTF8&pd_rd_i=0062315005&pd_rd_r=00dd8e12-eed5-4e1b-ae72-8ca0964f0f1f&pd_rd_w=7iexM&pd_rd_wg=bPkKf&pf_rd_p=fb1e266d-b690-4b4f-b71c-bd35e5395976&pf_rd_r=S9DGT9DP084FVS8MZND6&psc=1&refRID=S9DGT9DP084FVS8MZND6"),
+    Book(
         heading: "The Design of Everyday Things by Don Norman",
         disc:
             "The psychology and principles behind the design of everyday objects around us.",
